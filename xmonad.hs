f0f197ddc1d5dd7578444daace493b3be0986d28
Eric Kidd <git@randomhacks.net>
2010-11-02 21:50:01 -0400

    Switch to Pidgin
    It's more xmonad compatible than Empathy and file transfers work.

--- xmonad.hs
+++ xmonad.hs
@@ -19,7 +19,8 @@ myWorkspaces = ["1:code", "2:web", "3:im", "4", "5", "6", "7", "8:float", "9"]
 
 myManageHook = composeAll
   [ resource =? "Do" --> doIgnore
-  , resource =? "empathy" --> doShift "3:im"   -- Mysteriously fails.
+  , resource =? "Pidgin" --> doShift "3:im"
+  --, resource =? "empathy" --> doShift "3:im"   -- Mysteriously fails.
   , resource =? "xclock" --> doShift "3:im"    -- For testing.  It works.
   , resource =? "gimp-2.6" --> doShift "8:float"
   , manageHook gnomeConfig ]
@@ -48,7 +49,7 @@ workspaceLayouts =
     fixedLayout = FixedColumn 1 20 80 10
 
     -- A layout for instant messaging.
-    imLayout = withIM (1/6) (Title "Contact List") Grid
+    imLayout = withIM (1/6) (Title "Buddy List") Grid
 
     -- A simple floating-window layout.
     floatLayout = windowArrange simpleFloat
