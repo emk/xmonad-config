03bc9daef3877851289e160618be1db87ed74b07
Eric Kidd <git@randomhacks.net>
2010-11-03 09:48:09 -0400

    Place xmobar at the bottom of the screen

--- xmonad.hs
+++ xmonad.hs
@@ -1,7 +1,9 @@
+import System.IO
 import XMonad
 import XMonad.Actions.GridSelect
-import XMonad.Config.Desktop
 import XMonad.Config.Gnome
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
 import XMonad.Layout.FixedColumn
 import XMonad.Layout.Grid
 import XMonad.Layout.IM
@@ -13,6 +15,7 @@ import XMonad.Layout.WindowArranger
 import XMonad.ManageHook
 import qualified XMonad.StackSet as W
 import XMonad.Util.Replace
+import XMonad.Util.Run
 import XMonad.Util.EZConfig
 
 -- The names of my workspaces.  These are arbitrary, though I need to add
@@ -25,6 +28,7 @@ myManageHook = composeAll
   [ resource =? "Do"       --> doIgnore           -- Leave Gnome Do alone.
   , resource =? "Pidgin"   --> doShift "3:im"     -- Force to IM workspace.
   , resource =? "gimp-2.6" --> doShift "8:float"  -- Special-case the GIMP.
+  , manageDocks                                   -- For xmobar and gnome-panel.
   , manageHook gnomeConfig ]                      -- Gnome defaults.
 
 -- A standard tiled layout, with a master pane and a secondary pane off to
@@ -67,9 +71,10 @@ workspaceLayouts =
 
 -- Hook up my layouts.  We apply 'toggleLayouts' so that we can switch any
 -- window into or out of full-screen mode with a single command (see
--- below).  We use smartBorders to turn off the border around the focused
--- window if it's the only window on the screen.
-myLayout = smartBorders $ toggleLayouts Full workspaceLayouts
+-- below).  We use 'smartBorders' to turn off the border around the focused
+-- window if it's the only window on the screen.  And 'avoidStruts' leaves
+-- space for gnome-panel and xmobar.
+myLayout = avoidStruts $ smartBorders $ toggleLayouts Full workspaceLayouts
 
 -- Add new keys using EZConfig's Emacs-like syntax.  Note that "M-" means
 -- the xmonad modifier key, and not "Meta".
@@ -78,20 +83,36 @@ myKeys =
   , ("M-f", sendMessage ToggleLayout)       -- Toggle full-screen layout.
   ]
 
+-- Print the current xmonad status to a pipe for display by xmobar.
+-- Instead of using the usual xmobarPP configuration, we use defaultPP and
+-- override the colors to match the Ubuntu 10.10 "Clearlooks" theme.
+-- Note that we assume "position = Bottom" appears in your xmobar config,
+-- and that the Gnome bottom panel has been removed.
+myLogHook xmobarPipe = dynamicLogWithPP xmobarPrinter
+  where
+    xmobarPrinter = defaultPP
+      { ppOutput  = hPutStrLn xmobarPipe
+      , ppCurrent = xmobarColor "white" "#84a9d7" . wrap "[" "]"
+      , ppTitle   = xmobarColor "darkgreen"  "" . shorten 40
+      , ppVisible = wrap "(" ")"
+      , ppUrgent  = xmobarColor "red" "#84a9d7" }
+
 -- Create a new configuration structure.  I start with the standard
 -- 'gnomeConfig', override several fields, and then use EZConfig to patch
 -- up the key bindings.
-myConfig =
+myConfig xmobarPipe =
   gnomeConfig
     { workspaces = myWorkspaces
-    , layoutHook = desktopLayoutModifiers myLayout
+    , layoutHook = myLayout
     , manageHook = myManageHook
-    , modMask = mod3Mask }
+    , logHook    = myLogHook xmobarPipe
+    , modMask    = mod3Mask }
   `additionalKeysP` myKeys
 
 -- The main entry point for our window manager.  It's an ordinary Haskell
 -- program.  We specify 'replace' so that it will kill off any running
 -- window manager when it starts.
 main = do
-  replace
-  xmonad myConfig
+  replace                           -- Shut down current window manager.
+  xmobarPipe <- spawnPipe "xmobar"  -- Start xmobar panel.
+  xmonad $ myConfig xmobarPipe      -- Start xmonad with our config.
